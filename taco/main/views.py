c1e79d06d49ff7c571d1bb029e45a5387c5c502d
Scott <withorwithoutgod@gmail.com>
2015-04-05 19:14:47 -0500

    switch view to use shortcut

--- taco/main/views.py
+++ taco/main/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 
 def index(request):
-  template = loader.get_template('main/index.html')
-  return HttpResponse(template.render())
+  return render(request,"main/index.html")
